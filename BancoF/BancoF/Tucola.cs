ffc0559bfd6cee28097601f85c88b5192b33dfcb
Jorge Osuna <Jorgeosunaqui@gmail.com>
2019-03-26 17:33:06 -0700

    Asiasies

--- BancoF/BancoF/Tucola.cs
+++ BancoF/BancoF/Tucola.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BancoF
 {
-    public partial class Form1 : Form
+    public partial class frnVentanaPrincipal : Form
     {
-        public Form1()
+        public frnVentanaPrincipal()
         {
             InitializeComponent();
         }
