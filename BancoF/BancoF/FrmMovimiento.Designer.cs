b2e97782a79cd54462279107d9de7328184212fc
Oliver Rodriguez <oliver.rood.dev@gmail.com>
2019-03-27 03:35:47 +0100

    Form Principal

---
+++ BancoF/BancoF/FrmMovimiento.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace BancoF
+{
+    partial class FrmMovimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMovimientos = new System.Windows.Forms.Label();
+            this.tabMovimiento = new System.Windows.Forms.TabControl();
+            this.tabDeposito = new System.Windows.Forms.TabPage();
+            this.tabRetiro = new System.Windows.Forms.TabPage();
+            this.tabMovimiento.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbMovimientos
+            // 
+            this.lbMovimientos.AutoSize = true;
+            this.lbMovimientos.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMovimientos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.lbMovimientos.Location = new System.Drawing.Point(19, 23);
+            this.lbMovimientos.Name = "lbMovimientos";
+            this.lbMovimientos.Size = new System.Drawing.Size(183, 32);
+            this.lbMovimientos.TabIndex = 5;
+            this.lbMovimientos.Text = "Movimientos";
+            // 
+            // tabMovimiento
+            // 
+            this.tabMovimiento.Controls.Add(this.tabDeposito);
+            this.tabMovimiento.Controls.Add(this.tabRetiro);
+            this.tabMovimiento.Location = new System.Drawing.Point(25, 74);
+            this.tabMovimiento.Name = "tabMovimiento";
+            this.tabMovimiento.SelectedIndex = 0;
+            this.tabMovimiento.Size = new System.Drawing.Size(742, 284);
+            this.tabMovimiento.TabIndex = 6;
+            // 
+            // tabDeposito
+            // 
+            this.tabDeposito.Location = new System.Drawing.Point(4, 22);
+            this.tabDeposito.Name = "tabDeposito";
+            this.tabDeposito.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDeposito.Size = new System.Drawing.Size(734, 258);
+            this.tabDeposito.TabIndex = 0;
+            this.tabDeposito.Text = "Deposito";
+            this.tabDeposito.UseVisualStyleBackColor = true;
+            // 
+            // tabRetiro
+            // 
+            this.tabRetiro.Location = new System.Drawing.Point(4, 22);
+            this.tabRetiro.Name = "tabRetiro";
+            this.tabRetiro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRetiro.Size = new System.Drawing.Size(734, 258);
+            this.tabRetiro.TabIndex = 1;
+            this.tabRetiro.Text = "Retiro";
+            this.tabRetiro.UseVisualStyleBackColor = true;
+            // 
+            // FrmMovimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabMovimiento);
+            this.Controls.Add(this.lbMovimientos);
+            this.Name = "FrmMovimiento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmMovimientos";
+            this.Load += new System.EventHandler(this.FrmMovimiento_Load);
+            this.tabMovimiento.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMovimientos;
+        private System.Windows.Forms.TabControl tabMovimiento;
+        private System.Windows.Forms.TabPage tabDeposito;
+        private System.Windows.Forms.TabPage tabRetiro;
+    }
+}
--- BancoF/BancoF/Tucola.Designer.cs
+++ BancoF/BancoF/Tucola.Designer.cs
@@ -29,21 +29,102 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frnVentanaPrincipal));
+            this.btnMovimientos = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnCuentas = new System.Windows.Forms.Button();
+            this.btnCuentasBanco = new System.Windows.Forms.Button();
+            this.lblTecBank = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnMovimientos
+            // 
+            this.btnMovimientos.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnMovimientos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnMovimientos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMovimientos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnMovimientos.Location = new System.Drawing.Point(52, 224);
+            this.btnMovimientos.Name = "btnMovimientos";
+            this.btnMovimientos.Size = new System.Drawing.Size(392, 59);
+            this.btnMovimientos.TabIndex = 0;
+            this.btnMovimientos.Text = "Movimientos";
+            this.btnMovimientos.UseVisualStyleBackColor = false;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnClientes.Location = new System.Drawing.Point(52, 300);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(392, 59);
+            this.btnClientes.TabIndex = 1;
+            this.btnClientes.Text = "Clientes";
+            this.btnClientes.UseVisualStyleBackColor = false;
+            // 
+            // btnCuentas
+            // 
+            this.btnCuentas.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCuentas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCuentas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCuentas.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCuentas.Location = new System.Drawing.Point(52, 374);
+            this.btnCuentas.Name = "btnCuentas";
+            this.btnCuentas.Size = new System.Drawing.Size(392, 59);
+            this.btnCuentas.TabIndex = 2;
+            this.btnCuentas.Text = "Cuentas";
+            this.btnCuentas.UseVisualStyleBackColor = false;
+            // 
+            // btnCuentasBanco
+            // 
+            this.btnCuentasBanco.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnCuentasBanco.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCuentasBanco.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCuentasBanco.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCuentasBanco.Location = new System.Drawing.Point(52, 474);
+            this.btnCuentasBanco.Name = "btnCuentasBanco";
+            this.btnCuentasBanco.Size = new System.Drawing.Size(392, 59);
+            this.btnCuentasBanco.TabIndex = 3;
+            this.btnCuentasBanco.Text = "Cuentas Banco";
+            this.btnCuentasBanco.UseVisualStyleBackColor = false;
+            // 
+            // lblTecBank
+            // 
+            this.lblTecBank.AutoSize = true;
+            this.lblTecBank.Font = new System.Drawing.Font("Arial", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTecBank.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.lblTecBank.Location = new System.Drawing.Point(99, 78);
+            this.lblTecBank.Name = "lblTecBank";
+            this.lblTecBank.Size = new System.Drawing.Size(305, 78);
+            this.lblTecBank.TabIndex = 4;
+            this.lblTecBank.Text = "TecBank";
             // 
             // frnVentanaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(488, 562);
+            this.Controls.Add(this.lblTecBank);
+            this.Controls.Add(this.btnCuentasBanco);
+            this.Controls.Add(this.btnCuentas);
+            this.Controls.Add(this.btnClientes);
+            this.Controls.Add(this.btnMovimientos);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frnVentanaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMovimientos;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnCuentas;
+        private System.Windows.Forms.Button btnCuentasBanco;
+        private System.Windows.Forms.Label lblTecBank;
     }
 }
 
